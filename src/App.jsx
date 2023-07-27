317e7998bf1142ccc1f8063098d83fd64829aaeb
Vinivy Gabriel <vinivygabriel8@gmail.com>
2023-07-27 17:24:18 -0300

    Interactive Card

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <section className="bg">
+       <div className="onebox">
+         <div></div>
+       </div>
+       <div className="twobox">
+         <div></div>
+       </div>
+      </section>
+      <main>
+        <section className="Card">
+          <div className="cardFront">
+
+          </div>
+          <div className="cardBack">
+
+          </div>
+        </section>
+        <form >
+
+        </form>
+      </main>
+    </div>
+  )
+}
+
+export default App
