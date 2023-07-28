4da9c6d94599678e5b15fd92c447cbdc4d6d8b0e
Vinivy Gabriel <vinivygabriel8@gmail.com>
2023-07-28 16:13:09 -0300

    Interactive Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
 
+import CardFront from './Assets/images/bg-card-front.png'
+
 function App() {
   return (
     <div className="App">
@@ -14,15 +16,22 @@ function App() {
       </section>
       <main>
         <section className="Card">
-          <div className="cardFront">
-
+          <div className="Front">
           </div>
-          <div className="cardBack">
-
+          <div className="Back">
           </div>
         </section>
         <form >
-
+          <label htmlFor="holderName">CARDHOLDER NAME</label>
+          <input type="text" placeholder="Name"/>
+          <label htmlFor="CardNumber">CARD NUMBER</label>
+          <input type="text" />
+            <label htmlFor="">EXP. DATE (MM/YY)</label>
+          <div className="Date">
+            <input type="text" placeholder="DAY"/>
+            <input type="text" />
+            <input type="text" />
+          </div>
         </form>
       </main>
     </div>
