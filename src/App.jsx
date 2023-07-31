4bcc555d4ce12cb7735122eb0dd3db5f48992202
Vinivy Gabriel <vinivygabriel8@gmail.com>
2023-07-31 17:30:37 -0300

    Interactive Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import './App.css'
 
+import { useState } from 'react'
+
 import icon from './Assets/Svgs/card-logo.svg'
 
 function App() {
+  const [ Name , setName] = useState('')
+  const [ Number, setNumber] = useState()
+
+
+  function CardFrontName(NameCard) {
+    NameCard.preventDefault()
+
+    let NameCardFrontExib = document.querySelector('.TranspName') 
+    NameCardFrontExib.textContent = Name
+
+    if()
+  }
+
+  
+
+  function CardFrontNumber(NumeroCard) {
+    NumeroCard.prenventDefault()
+  }
+  
+
+  function Concluido() {
+    
+
+    let NumberFrontExib = document.querySelector('.TranspNumber')
+    NumberFrontExib.textContent = Number
+
+    alert(Name + " " + Number)
+  }
+ 
   return (
-    <div className="App">
+    <div className="App" >
       <section className="bg">
        <div className="onebox">
          <div></div>
@@ -20,7 +51,7 @@ function App() {
             <section className="TimeReal">
               <span className="TranspNumber">0000 0000 0000 0000</span>
               <div >
-                <span className="TranspName">Jane Appleseed</span> 
+                <span className="TranspName">JANE APPLESEED</span> 
                 <span className="TranspDate">00/00</span>
               </div>
             </section>
@@ -33,31 +64,54 @@ function App() {
         </section>
 
         <form >
-          <div  className="columnOne">
+          <div  className="columnOne" onSubmit={CardFrontName}>
            <label htmlFor="holderName">CARDHOLDER NAME</label>
-           <input type="text" className="Name" placeholder="e.g. Jane Aplleseed"/>
+           <input 
+            type="text" 
+            className="Name"
+            placeholder="e.g. Jane Aplleseed"
+            value={Name}
+            onChange={NameCard => setName(NameCard.target.value)}
+           />
+           <span className="DefaultName"></span>
           </div>
 
-          <div  className="columnOne">
+          <div  className="columnOne" onSubmit={CardFrontNumber}>
             <label htmlFor="CardNumber">CARD NUMBER</label>
-            <input type="text" className="Number" placeholder="e.g 1234 5678 9123 0000"/>
+            <input 
+             type="text"
+             className="Number" 
+             placeholder="e.g 1234 5678 9123 0000"
+             value={Number}
+             onChange={NumeroCard => setNumber(NumeroCard.target.value)}/>
+             <span className="DefaultNumber"></span>
           </div>
 
           <section className="Date">
             <div className="labes">
-               <label htmlFor="">EXP. DATE (MM/YY)</label>
-               <label htmlFor="CVC">CVC</label>
-             </div>
+              <label htmlFor="">EXP. DATE (MM/YY)</label>
+              <label htmlFor="CVC">CVC</label>
+            </div>
 
            <div className="state">
               <div className="Info">
-               <input type="text" className="Day" placeholder="DAY"/>
-               <input type="text" className="Day"  placeholder="MM"/>
-               <input type="text" className="Cvc" placeholder='CVC'/>
+               <input type="text" 
+                className="Day" 
+                placeholder="DAY"/>
+               <input type="text" 
+                className="Day"  
+                placeholder="MM"/>
+               <input type="text" 
+                className="Cvc" 
+                placeholder='CVC'/>
               </div>
+              <span className="DefaultDate">oaiao</span>
             </div>
-            <button className="Confirm">Confirm</button>
-
+            
+            <button 
+            onClick= {Concluido}
+            className="Confirm">Confirm</button>
+           
           </section>
         </form>
       </main>
